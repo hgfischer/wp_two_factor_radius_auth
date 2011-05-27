844f0fe45067839116b6d68b1004cdc7d2629ab5
Herbert G. Fischer <hgfischer@sp.r7.com>
2011-05-27 15:57:03 -0300

    fixing settings link

--- two-factor-radius.php
+++ two-factor-radius.php
@@ -94,7 +94,7 @@ class TwoFactorRadiusAuth
 	 */
 	function filterPluginActions($links)
 	{
-		$settings_link = '<a href="options-general.php?page=radius-auth">' . __( 'Settings' ) . '</a>';
+		$settings_link = '<a href="options-general.php?page=two-factor-radius">' . __( 'Settings' ) . '</a>';
 		array_unshift($links, $settings_link);
 		return $links;
 	}
